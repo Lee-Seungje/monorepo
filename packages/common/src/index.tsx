d6b515977a1e3c1c89d09bc3bd84862ff74c0abb
Lee-Seungje <moondgod@gmail.com>
2023-08-10 22:18:55 +0900

    Add admin, common

---
+++ packages/common/src/index.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from "react";
+import { primary } from "common/color";
+
+export const Button = (): ReactElement => {
+  return <button style={{ backgroundColor: primary }}>누르세요</button>;
+};
