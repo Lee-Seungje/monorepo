310dd8740cfb769cc1a43185b6ed64cb8688f780
Lee-Seungje <moondgod@gmail.com>
2023-08-10 22:36:53 +0900

    Add client

---
+++ packages/client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+import { Button } from "@common/components";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Button />
+      client입니다.
+    </main>
+  );
+}
